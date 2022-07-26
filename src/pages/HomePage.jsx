2d6cc794c5481ecb21165e164108e2566827dfac
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-07-26 17:32:05 +0100

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,16 @@
+import "./HomePage.css";
+import logo from "../images/logo.png";
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="project-bar">Projects</div>
+      <div className="resume-bar">Resume</div>
+      <hr className="horizontal-line"></hr>
+    </div>
+  );
 }
 
 export default HomePage;
