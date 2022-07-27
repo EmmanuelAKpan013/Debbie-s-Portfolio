1602de1bf4d5b971536dd643c71a85f90140f351
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-07-27 15:32:13 +0100

    update

--- src/pages/NomadPage.jsx
+++ src/pages/NomadPage.jsx
@@ -1,5 +1,34 @@
+import "./NomadPage.css";
+import Navbar from "../components/Navbar";
+import nomadLogo from "../images/nomad/nomad.png";
+import image1 from "../images/nomad/Rectangle 469.png";
+import image2 from "../images/nomad/Rectangle 498.png";
+import image3 from "../images/nomad/Rectangle 499.png";
+import image4 from "../images/nomad/Rectangle 500.png";
+
 function NomadPage() {
-  return <div>NomadPage</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        {/* <div className="box-1">
+          <img src={nomadLogo} alt="" />
+        </div> */}
+        <div className="box-1">
+          <img src={image1} alt="" />
+        </div>
+        <div className="box-1">
+          <img src={image2} alt="" />
+        </div>
+        <div className="box-1">
+          <img src={image3} alt="" />
+        </div>
+        <div className="box-1">
+          <img src={image4} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NomadPage;
