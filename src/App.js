38372f369d132b22b1ae8d8ed47b487067661084
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-07-27 09:31:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
 import NomadPage from "./pages/NomadPage";
+import BlomBlomPage from "./pages/BlomBlomPage";
+import SaudipixPage from "./pages/SaudipixPage";
+import InvestNowPage from "./pages/InvestNowPage";
+import WaterTalkPage from "./pages/WaterTalkPage";
 
 function App() {
   return (
@@ -10,10 +14,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/project" element={<ProjectPage />} />
-          <Route path="/project/:pageId" element={<NomadPage />} />
-          {/* <Route path="/project/:pageId" element={<ProjectPage />} /> */}
-          {/* <Route path="/project/:pageId" element={<ProjectPage />} /> */}
-          {/* <Route path="/project/:pageId" element={<ProjectPage />} /> */}
+          <Route path="/project/nomadbank" element={<NomadPage />} />
+          <Route path="/project/blomblom" element={<BlomBlomPage />} />
+          <Route path="/project/saudipix" element={<SaudipixPage />} />
+          <Route path="/project/investnow" element={<InvestNowPage />} />
+          <Route path="/project/watertalk" element={<WaterTalkPage />} />
         </Routes>
       </Router>
     </>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,28 +1,56 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./ProjectPage.css";
 import Navbar from "../components/Navbar";
 
 function ProjectPage() {
-  const params = useParams();
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/project/{params.pageId}`);
-  };
 
   return (
     <>
       <Navbar />
       <div className="projectpage-container">
         <div className="project-header">selected projects</div>
-        <div className="project-list">
-          <div className="project-item item1" onClick={handleClick}>
+        <div className="project-lists">
+          <div
+            className="project-list item1"
+            onClick={() => {
+              navigate(`/project/nomadbank`);
+            }}
+          >
             Fastr - NomadBank
           </div>
-          <div className="project-item item2">Website Redesign - Bloobloom</div>
-          <div className="project-item item3">Saudipix</div>
-          <div className="project-item item4">InvestNow.ng Redesign</div>
-          <div className="project-item item5">WaterTalk</div>
+          <div
+            className="project-list item2"
+            onClick={() => {
+              navigate(`/project/blomblom`);
+            }}
+          >
+            Website Redesign - Bloobloom
+          </div>
+          <div
+            className="project-list item3"
+            onClick={() => {
+              navigate(`/project/saudipix`);
+            }}
+          >
+            Saudipix
+          </div>
+          <div
+            className="project-list item4"
+            onClick={() => {
+              navigate(`/project/investnow`);
+            }}
+          >
+            InvestNow.ng Redesign
+          </div>
+          <div
+            className="project-list item5"
+            onClick={() => {
+              navigate(`/project/watertalk`);
+            }}
+          >
+            WaterTalk
+          </div>
         </div>
       </div>
     </>
