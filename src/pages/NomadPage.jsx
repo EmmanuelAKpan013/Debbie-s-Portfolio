7ee0b7f0846a9cb9135c5b9c2b27d9606508e471
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-01 18:08:08 +0100

    update

--- src/pages/NomadPage.jsx
+++ src/pages/NomadPage.jsx
@@ -155,14 +155,17 @@ function NomadPage() {
             <h2>Research</h2>
           </div>
           <div className="research-image">
-            <div className="image-box-1">
-              Box 1{/* <img src={researchLadyOne} alt="" /> */}
+            <div className="imgae-box image-box-1">
+              {/* Box 1 */}
+              <img src={researchLadyOne} alt="" />
             </div>
-            <div className="image-box-2">
-              Box 2{/* <img src={researchLadyTwo} alt="" /> */}
+            <div className=" imgae-box image-box-2">
+              {/* Box 2 */}
+              <img src={researchManOne} alt="" className="manImage" />
             </div>
-            <div className="image-box-3">
-              Box 3{/* <img src={researchManOne} alt="" /> */}
+            <div className="imgae-box image-box-3">
+              {/* Box 3 */}
+              <img src={researchLadyTwo} alt="" />
             </div>
           </div>
         </div>
