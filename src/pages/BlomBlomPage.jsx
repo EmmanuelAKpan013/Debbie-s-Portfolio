8a70ea54aca63770f83757ef68449364ea7176b2
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-08 22:32:57 +0100

    update

--- src/pages/BlomBlomPage.jsx
+++ src/pages/BlomBlomPage.jsx
@@ -1,3 +1,6 @@
+import blomblomLogo from "../images/blomblom/Group 1421.png";
+import blomblomFirstImage from "../images/blomblom/Rectangle 469 (1).png";
+
 function BlomBlomPage() {
   return <div>BlomBlomPage</div>;
 }
