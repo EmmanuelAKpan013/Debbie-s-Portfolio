ecca0ad64bc32e3a97bfaf2b4fa0badeebce6fc6
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-08 23:05:21 +0100

    update

--- src/pages/BlomBlomPage.jsx
+++ src/pages/BlomBlomPage.jsx
@@ -1,8 +1,22 @@
+import Navbar from "../components/Navbar";
+import "./BlomBlomPage.css";
 import blomblomLogo from "../images/blomblom/Group 1421.png";
-import blomblomFirstImage from "../images/blomblom/Rectangle 469 (1).png";
+import blomblomFirstImage from "../images/blomblom/Rectangle 469 (2).png";
 
 function BlomBlomPage() {
-  return <div>BlomBlomPage</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="blomLandingImageAndLogo">
+        {/* <div className="blomblom-logo">
+          <img src={blomblomLogo} alt="" />
+        </div> */}
+        <div className="blomblomLandingImg">
+          <img src={blomblomFirstImage} alt="" className="blomFirstImage" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BlomBlomPage;
