5a7e1e53c44d538d3816e35d97d214e0b95313c1
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-09 01:19:42 +0100

    update

--- src/pages/BlomBlomPage.jsx
+++ src/pages/BlomBlomPage.jsx
@@ -8,11 +8,32 @@ function BlomBlomPage() {
     <>
       <Navbar />
       <div className="blomLandingImageAndLogo">
-        {/* <div className="blomblom-logo">
-          <img src={blomblomLogo} alt="" />
-        </div> */}
         <div className="blomblomLandingImg">
           <img src={blomblomFirstImage} alt="" className="blomFirstImage" />
+        </div>
+        <div className="blomblom-logo">
+          <img src={blomblomLogo} alt="" />
+        </div>
+      </div>
+      <div className="roleContainer">
+        <div className="roleContent">
+          <h3>ROLE</h3>
+          <br />
+          <p>Research, UX Design</p>
+          <p> UI Design.</p>
+        </div>
+        <div className="roleContent">
+          <h3>TASK</h3>
+          <br />
+          <p>
+            E- commerce Website Redesign, <br /> Digital solution for walk-in
+            store and seamsless <br /> end to end appointment scheduling.
+          </p>
+        </div>
+        <div className="roleContent year">
+          <h3>YEAR</h3>
+          <br />
+          <p>2021 - Present</p>
         </div>
       </div>
     </>
