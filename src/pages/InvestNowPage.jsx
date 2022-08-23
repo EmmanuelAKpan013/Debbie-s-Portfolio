f8df41d20ae9ec2cccff97646eedc593e57d8764
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-23 22:06:23 +0100

    update

--- src/pages/InvestNowPage.jsx
+++ src/pages/InvestNowPage.jsx
@@ -2,6 +2,12 @@ import Navbar from "../components/Navbar";
 import "./InvestNowPage.css";
 import investNowLandingImage from "../images/investnow/Component 12.png";
 import investNowLogo from "../images/investnow/investNowLogo.png";
+import investNowFirstIphoneDesign from "../images/investnow/Group 9376.png";
+import investNowSecondIphoneDesign from "../images/investnow/Group 9375.png";
+import investNowThirdIphoneDesign from "../images/investnow/Group 9381.png";
+import investNowFourthIphoneDesign from "../images/investnow/Group 9380.png";
+import investNowFifthIphoneDesign from "../images/investnow/Group 9377.png";
+import investNowSixthIphoneDesign from "../images/investnow/Group 9374.png";
 
 function InvestNowPage() {
   return (
@@ -39,7 +45,36 @@ function InvestNowPage() {
           <p>3 days</p>
         </div>
       </div>
-      <div className="investNow-layoutDesign"></div>
+      <div className="investNow-layoutDesign">
+        <div>
+          <img src={investNowFirstIphoneDesign} alt="" />
+        </div>
+        <div>
+          <img src={investNowSecondIphoneDesign} alt="" />
+        </div>
+        <div>
+          <img src={investNowThirdIphoneDesign} alt="" />
+        </div>
+        <div>
+          <img src={investNowFourthIphoneDesign} alt="" />
+        </div>
+        <div>
+          <img src={investNowFifthIphoneDesign} alt="" />
+        </div>
+        <div>
+          <img src={investNowSixthIphoneDesign} alt="" />
+        </div>
+        <div>
+          <h3>InvestNow</h3>
+          <br />
+          <p>
+            Investnow is an Integrated online web and mobile apps platform
+            powered by United Capital Plc, that enables you to have complete
+            management over your investment and financial portfolio to maximize
+            profits and cut down losses.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
