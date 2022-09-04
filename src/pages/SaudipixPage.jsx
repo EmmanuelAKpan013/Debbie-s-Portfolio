1e0fe0ec435bc507a72a525840d826e73abb276e
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-04 21:59:12 +0100

    update

--- src/pages/SaudipixPage.jsx
+++ src/pages/SaudipixPage.jsx
@@ -1,11 +1,25 @@
+import "./SaudipixPage.css";
 import Navbar from "../components/Navbar";
 import Pagination from "../components/Pagination";
-import "./SaudipixPage.css";
+import saudipixLandingImage from "../images/saudipix/Hero Section.png";
+import saudipixLogo from "../images/saudipix/Saudipix.png";
 
 function SaudipixPage() {
   return (
     <>
       <Navbar />
+      <div className="saudipixLandingImageAndLogo">
+        <div className="saudipixLandingImg">
+          <img
+            src={saudipixLandingImage}
+            alt=""
+            className="saudipixFirstImage"
+          />
+        </div>
+        <div className="saudipix-logo">
+          <img src={saudipixLogo} alt="" />
+        </div>
+      </div>
     </>
   );
 }
