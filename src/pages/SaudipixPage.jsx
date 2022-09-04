2a4645024e185f09aef97c153418eeba53ce2a59
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-04 22:09:25 +0100

    update

--- src/pages/SaudipixPage.jsx
+++ src/pages/SaudipixPage.jsx
@@ -20,6 +20,26 @@ function SaudipixPage() {
           <img src={saudipixLogo} alt="" />
         </div>
       </div>
+      <div className="saudipix-roleContainer">
+        <div className="saudipix-roleContent">
+          <h3>ROLE</h3>
+          <br />
+          <p>UI Design, UX Design</p>
+        </div>
+        <div className="saudipix-roleContent">
+          <h3>TASK</h3>
+          <br />
+          <p>
+            Improve the userinterface and <br /> experience of the onboarding
+            process
+          </p>
+        </div>
+        <div className="saudipix-roleContent year">
+          <h3>YEAR</h3>
+          <br />
+          <p>3 days</p>
+        </div>
+      </div>
     </>
   );
 }
