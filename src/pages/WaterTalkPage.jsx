704eeedfb7b6cd9cb849c7a745133b4a3aa224fe
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-08 12:16:49 +0100

    update

--- src/pages/WaterTalkPage.jsx
+++ src/pages/WaterTalkPage.jsx
@@ -1,5 +1,22 @@
+import "./WaterTalkPage.css";
+import Navbar from "../components/Navbar";
+import waterLandingImage from "../images/watertalk/waterFirstImg.png";
+import waterLogo from "../images/watertalk/Water.png";
+
 function WaterTalkPage() {
-  return <div>WaterTalkPage</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="waterLandingImageAndLogo">
+        <div className="waterLandingImg">
+          <img src={waterLandingImage} alt="" className="waterFirstImage" />
+        </div>
+        <div className="water-logo">
+          <img src={waterLogo} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default WaterTalkPage;
